42246fe6355276af58101715ea4a5ddcd45c2b5f
kannan-mayoo <mkanna79@gmail.com>
2025-10-09 01:35:45 +0530

    Updating Fix

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -6,7 +6,6 @@ import UserList from "./components/UserList";
 export default async function UsersLayout({children}:{children:React.ReactNode}) {
     const users = await getUsers();
     return (
-        //@ts-expect-error Server Component
         <Sidebar>
             <div className='h-full' >
                 <UserList items={users} />
