a9f48a10b06f48574eab4920a8b8baeb728be7a1
kannan-mayoo <mkanna79@gmail.com>
2025-10-09 00:33:58 +0530

    Updating Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = localFont({
     { path: '/fonts/Inter-Regular.woff2', weight: '400', style: 'normal' },
     { path: '/fonts/Inter-Bold.woff2', weight: '700', style: 'normal' }
   ],
-  variable: '--font-inter',
+  // variable: '--font-inter',
   display: 'swap'
 });
 
